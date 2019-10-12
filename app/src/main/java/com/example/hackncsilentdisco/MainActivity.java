870dbac2ee66bb5bafaf08b36564d5a9b5083fde
dylanbinley <dylanbinley@gmail.com>
2019-10-12 16:52:30 -0400

    framework of basic app :P

--- app/src/main/java/com/example/hackncsilentdisco/MainActivity.java
+++ app/src/main/java/com/example/hackncsilentdisco/MainActivity.java
@@ -17,8 +17,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+int correct =0;
+int attempted = 0;
+String word ="the";
+String translation = "el";
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +45,76 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new OnClickListener() {
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        textView = (TextView) findViewById(R.id.textView1);
+
+        button1.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View view) {
-               // Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-                Button button = (Button) findViewById(R.id.button1);
-                button.setText("Clicked !");
+               clicked(button1);
             }
 
         });
 
+        button2.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                clicked(button2);
+            }
+
+        });
+
+        button3.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                clicked(button3);
+            }
+
+        });
+
+        button4.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                clicked(button4);
+            }
+
+        });
+
+    }
+
+
+
+    public void generateNewQuestion() {
+        //word = generateword()
+        //translation = generatetranslation(word)
+        textView.setText(word);
+        //use math.random to determine which button has correct answer next
+        //set other three to random incorrect translations
+
+        /*button1.setText();
+        button1.setText();
+        button1.setText();
+        */
+   }
+
+    public void clicked(Button button) {
+        if (button.getText().equals(translation))
+        {
+            ++correct;
+        }
+        ++attempted;
+        generateNewQuestion();
     }
 
 }
