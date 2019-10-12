f39ef319ecbc0e9e92fb6f85b1357cf0eaf0f33d
dylanbinley <dylanbinley@gmail.com>
2019-10-12 13:40:11 -0400

    yoooooooooo... barely works after 3 hours

--- app/src/main/java/com/example/hackncsilentdisco/MainActivity.java
+++ app/src/main/java/com/example/hackncsilentdisco/MainActivity.java
@@ -10,6 +10,14 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                Button button = (Button) findViewById(R.id.button1);
+                button.setText("Clicked !");
+            }
+
+        });
+
     }
 
 }
+
